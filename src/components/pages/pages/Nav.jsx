f1a4e01be98b8e1610ab87b218522f55447fc964
unknown <tompsherman@gmail.com>
2020-09-23 19:56:01 -0400

    component scaffolding

---
+++ src/components/pages/pages/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <p>Nav Component</p>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/components/pages/pages/Short.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Short = () => {
+    return (
+        <div>
+            <p>Short Component</p>
+        </div>
+    )
+}
+
+export default Short
