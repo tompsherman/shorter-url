d7fe6efecf3fb436b69c931f9aa328b804a69d5a
unknown <tompsherman@gmail.com>
2020-09-24 20:21:19 -0400

    react hook form

--- src/components/pages/Short.jsx
+++ src/components/pages/Short.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {useForm} from "react-hook-form"
 
 const Short = () => {
+    const {handleSubmit, register, errors} = useForm()
+    const onShort = link => console.log(link)
+
     return (
         <div> 
-            <input type="text" />
-             <button> Shorten It!</button>
+            <input 
+                type="text"
+                name="urlShort" 
+                placeholder="enter your URL"
+                ref={register({
+                    required: "Required",
+                    pattern: {
+                        // value: "",
+                        message: "please enter a URL"
+                    }
+                })}
+            />
+            {errors.urlShort && errors.urlShort.message}
+             <button type="submit"> Shorten It!</button>
         </div>
     )
 }
